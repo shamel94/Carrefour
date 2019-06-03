c44bb1fc2c977aa9972d446b3d68d4c8859747aa
shamel94 <51302965+shamel94@users.noreply.github.com>
2019-06-03 06:08:33 +0200

    Update runIt.bat

--- script/runIt.bat
+++ script/runIt.bat
@@ -1,2 +1,21 @@
-if not exist "../data/output" mkdir ../data/output
-java -jar ../bin/phenix-challenge.jar ../data/input/ ../data/output/
+@echo off
+IF %1.==. GOTO No1
+IF %2.==. GOTO No2
+
+set input_dir=%1
+set output_dir=%2
+
+java -jar ../bin/phenix-challenge.jar %input_dir% %output_dir%
+
+GOTO End1
+
+:No1
+  ECHO input directory missing
+GOTO End1
+:No2
+  ECHO output directory missing
+GOTO End1
+
+:End1
+
+
